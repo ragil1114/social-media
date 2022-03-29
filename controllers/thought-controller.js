982f6de3e3a5319bf3f73c71152b4b4191f6aa57
Ragil14 <rahil.gilani@gmail.com>
2022-03-29 12:19:26 -0500

    clean code

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -18,7 +18,7 @@ const thoughtController = {
           res.status(404).json({ message: 'No user found with this id!' });
           return;
         }
-        res.json(dbUserData);
+        res.json({ message: 'Thought added!' });
       })
       .catch(err => res.json(err));
   },
@@ -71,6 +71,23 @@ const thoughtController = {
       { new: true }
     )
       .then(dbUserData => res.json(dbUserData))
+      .catch(err => res.json(err));
+  },
+
+  // update thought
+  updateThought({ params, body }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $set: body },
+      { new: true }
+    )
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
       .catch(err => res.json(err));
   }
 };
