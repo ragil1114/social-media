c3930ee611c16180ec76f54377bc084b5b3ae935
Ragil14 <rahil.gilani@gmail.com>
2022-03-29 12:18:50 -0500

    update Thought Schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,51 +1,25 @@
 const { Schema, model, } = require('mongoose');
+const ReactionSchema = require('./Reaction');
 const dateFormat = require('../utils/dateFormat');
 
 const ThoughtSchema = new Schema(
   {
     thoughtText: {
-      type: string = '',
-      //required: true,
-      //minlength: 1,
-      //maxlength: 280,
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      get: createdAtVal => dateFormat(createdAtVal)
-    },
-    username: {
-      type: string,
-      //required: true,
-    },
-  },
-  {
-    toJSON: {
-      getters: true
-    }
-  }
-);
-
-const ReactionSchema = new Schema(
-  {
-    reactionId: {
-      type: Schema.Types.ObjectId,
-      default: () => new Types.ObjectId()
-    },
-    reactionBody: {
       type: String,
       required: true,
-      trim: true
-    },
-    username: {
-      type: string,
-      //required: true,
+      minlength: 1,
+      maxlength: 280,
     },
     createdAt: {
       type: Date,
       default: Date.now,
       get: createdAtVal => dateFormat(createdAtVal)
-    }
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [ReactionSchema],
   },
   {
     toJSON: {
@@ -56,7 +30,7 @@ const ReactionSchema = new Schema(
 
 // Virtual for ThoughtSchema to get the total reaction count
 ThoughtSchema.virtual('reactionCount').get(function() {
-  return this.replies.length;
+  return this.reactions.length;
 });
 
 const Thought = model('Thought', ThoughtSchema);
